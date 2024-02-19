95b136b1e5668d828c7ea0118b3f4a94210266ba
Manoj <kamriyamanoj@gmail.com>
2024-02-19 22:27:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'; // Import CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img id="logo" src="white_logo.png" width="170px" height="170px" alt="Logo" />
+      <div id="name_1">PARIKRANTI FOUNDATION</div>
+
+      <div id="boxrow1">
+        <div className="item" id="box1">The united social federation</div>
+        <div className="item" id="box2">The social panchayat</div>
+        <div className="item" id="box3">The X fundamental</div>
+        <div className="item" id="box4">The Ultra Bharat</div>
+      </div>
+
+      <div id="boxrow2">
+        <div className="item" id="box5">The Big Heart</div>
+        <div className="item" id="box6">The X Founders</div>
+        <div className="item" id="box7">The Holistic Incubation</div>
+        <div className="item" id="box8">The X solution pvt ltd</div>
+      </div>
+
+      <button className="btn">Get Involved</button>
     </div>
   );
 }
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,16 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+const sendToAnalytics = ({ name, delta, id }) => {
+  // Implement your analytics reporting here
+  console.log({ name, delta, id });
+};
+
+export default function reportWebVitals(onPerfEntry) {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
-    });
+    }
   }
-};
-
-export default reportWebVitals;
